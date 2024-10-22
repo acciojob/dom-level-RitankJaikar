6c36128158c29d89da6f89fa5f732c5033d0d8f9
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-22 15:24:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let depth=1;
+function domLevelFinder(ele) {
+	if(ele.parentElement !== null) {
+		depth++;
+		//console.log(ele.parentElement.nodeName);
+		domLevelFinder(ele.parentElement);	
+	}
+	else {
+		console.log(depth);
+		return depth;
+	}
+}
+
+const ele= document.querySelector("#level");
+domLevelFinder(ele);
+
+
+
+
